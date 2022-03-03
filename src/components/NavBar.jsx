ae5accdf777931549a5ce59301ea4d003649be22
Ome Asraf <omeasraf0@gmail.com>
2022-03-03 11:26:09 -0500

    Completed NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,6 +2,8 @@ import "../css/navbar.css";
 import UnsplashIcon from "../icons/UnsplashIcon";
 
 import { HiMenu } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
+import { RiCameraLensFill } from "react-icons/ri";
 
 const NavBar = () => {
   return (
@@ -9,17 +11,33 @@ const NavBar = () => {
       <div className="icon">
         <UnsplashIcon />
       </div>
-      <div className="search">
-        <input type="text" name="search" />
-      </div>
+      <div style={{ width: "18px" }}></div>
+      <form action="/search" className="search-form">
+        <button>
+          <BiSearch />
+        </button>
+        <div className="searchField">
+          <input
+            type="search"
+            className="search"
+            placeholder="Search free high-resolution photos"
+          />
+        </div>
+
+        <div className="searchByImage">
+          <button>
+            <RiCameraLensFill />
+          </button>
+        </div>
+      </form>
       <div className="actionButtons">
         <button>Explore</button>
         <button>Advertise</button>
         <button>Blog</button>
       </div>
       <div className="vertical-line"></div>
-      <div className="calloutButtons">
-        <button>Login</button>
+      <div className="actionButtons">
+        <button>Log in</button>
         <button className="submitButton">Submit a photo</button>
         <button className="menuButton">
           <HiMenu />
